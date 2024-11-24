cd7ebd2820e3073c118413154da7553570634207
Whitedevi5776 <149379248+Whitedevi5776@users.noreply.github.com>
2024-11-24 08:10:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
 global.pairingNumber = process.env.BOT_NUMBER || '2348123999085';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'jcIUhbQZ#WZiPEuBbtifAnrfxS6aZj1DBndhaVCMM7iE-5LfE-Go';  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'iYIyTSyA#23my8cDXz8KR7-TKAlMjQXxZMJkYbEy-D24F9oxFKKE';  // put your session id here
 
 global.mods = []
 global.prems = []
